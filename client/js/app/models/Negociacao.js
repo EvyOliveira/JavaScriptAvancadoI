c5919f7079402311795a2bdc03e5d87dce6cb195
EvyOliveira <cristinioliveira.eo@gmail.com>
2020-03-04 16:48:46 -0300

    fix: trabalhando com datas

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -3,6 +3,7 @@ class Negociacao {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this);
     }
 
     get ObtemVolume() {
@@ -10,7 +11,7 @@ class Negociacao {
    }
 
    get Data() {
-       return this._data;
+       return new Date(this._data.getTime());
    }
 
    get Quantidade() {
